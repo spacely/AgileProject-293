29ebaa7298f16285e0a415085723f489d9740831
Jesse Cirimelli-Low <jcirimel@ucsc.edu>
2022-02-27 13:54:24 -0800

    progress on fixed point inverter scala model

--- src/test/scala/Inverse/MatrixModelInverse.scala
+++ src/test/scala/Inverse/MatrixModelInverse.scala
@@ -1,9 +1,15 @@
 package gps
 import scala.collection.mutable.ArrayBuffer
+import spire._
+import spire.math._
+import spire.implicits._
+import spire.math.extras.{FixedPoint, FixedScale}
 
-object MatrixInverseModel {
-    type Matrix = Seq[Seq[Int]]
-    def genIdentity(n: Int): Matrix = Seq.tabulate(n,n) { (i,j) => if (i==j) 1 else 0 }
+class MatrixInverseModel(val p: GPSParams) {
+    type Matrix = Seq[Seq[FixedPoint]]
+    implicit val scale = p.scale
+    
+    def genIdentity(n: Int): Matrix = Seq.tabulate(n,n) { (i,j) => if (i==j) FixedPoint(1) else FixedPoint(0) }
     def append(a: Matrix, b: Matrix): Matrix = {
         assert(a.size == b.size)
         assert(a.head.size == b.head.size)
@@ -16,13 +22,13 @@ object MatrixInverseModel {
         c
     }
     def gaussElimination(a: Matrix, y: Matrix): Matrix = {
-        val A = ArrayBuffer.fill(a.size)(ArrayBuffer.fill(a.size)(0))
+        val A = ArrayBuffer.fill(a.size)(ArrayBuffer.fill(a.size)(FixedPoint(0.0)))
         for (i <- 0 until a.size) {
             for (j <- 0 until a.size) {
                 A(i)(j) = a(i)(j)
             }
         }
-        val Y = ArrayBuffer.fill(y.size)(ArrayBuffer.fill(y.size)(0))
+        val Y = ArrayBuffer.fill(y.size)(ArrayBuffer.fill(y.size)(FixedPoint(0.0)))
         for (i <- 0 until y.size) {
             for (j <- 0 until y.size) {
                 Y(i)(j) = y(i)(j)
@@ -30,8 +36,9 @@ object MatrixInverseModel {
         }
         for(i <- 0 until A.length){
             for(j <- i+1 until A.length){
-                var ratio = A(j)(i)/A(i)(i)
-                A(j)(i) = 0			
+                var ratio = FixedPoint(0.0)
+                ratio = (A(j)(i)/A(i)(i))
+                A(j)(i) = FixedPoint(0.0)			
                 for(k <- i+1 until A.length){
                     A(j)(k) -= ratio*A(i)(k)
                 }
@@ -40,8 +47,8 @@ object MatrixInverseModel {
                 }
             }
         }
-        //var X = new Array[Double](A.length)
-        val X = ArrayBuffer.fill(y.size)(ArrayBuffer.fill(y.size)(0))
+
+        val X = ArrayBuffer.fill(y.size)(ArrayBuffer.fill(y.size)(FixedPoint(0.0)))
         for(i <- A.length-1 to 0 by -1){
             for (j <- 0 until X(0).length) {
                 X(i)(j) = Y(i)(j)
--- src/test/scala/Inverse/MatrixModelInverseTest.scala
+++ src/test/scala/Inverse/MatrixModelInverseTest.scala
@@ -3,18 +3,25 @@ import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
 
-import gps.MatrixInverseModel.Matrix
+import spire._
+import spire.math._
+import spire.implicits._
+import spire.math.extras.{FixedPoint, FixedScale}
+
 
 object MatrixInverseData {
-    val ident2x2  = Seq(Seq(1,0),
-                     Seq(0,1))
+    type Matrix = Seq[Seq[FixedPoint]]
+    val p = GPSParams()
+    implicit val scale = p.scale
+    val ident2x2  = Seq(Seq(FixedPoint(1.0),FixedPoint(0.0)),
+                     Seq(FixedPoint(0.0),FixedPoint(1.0)))
 
-    val in2x2  = Seq(Seq(1,2),
-                     Seq(3,4))
-    val out2x4 = Seq(Seq(1,2,1,0),
-                     Seq(3,4,0,1))
-    val outgauss2x4 = Seq(Seq(1,0,   -2,1),
-                          Seq(0,1,   1,0))
+    val in2x2  = Seq(Seq(FixedPoint(1.5),FixedPoint(2.5)),
+                     Seq(FixedPoint(3.5),FixedPoint(4.5)))
+    val out2x4 = Seq(Seq(FixedPoint(1.5),FixedPoint(2.5),FixedPoint(1.0),FixedPoint(0.0)),
+                     Seq(FixedPoint(3.5),FixedPoint(4.5),FixedPoint(0.0),FixedPoint(1.0)))
+    val outgauss2x4 = Seq(Seq(FixedPoint(1.0),FixedPoint(0.0),   FixedPoint(-2.25),FixedPoint(1.25)),
+                          Seq(FixedPoint(0.0),FixedPoint(1.0),   FixedPoint(1.75),FixedPoint(-0.75)))
 
 }
 
@@ -22,16 +29,13 @@ class MatInverseModelTester extends AnyFlatSpec with ChiselScalatestTester {
   behavior of "MatrixInverseModel"
 
   it should "Generate Ident Matrix 2x2" in {
-    assert(MatrixInverseModel.genIdentity(2) == MatrixInverseData.ident2x2)
+    assert(new MatrixInverseModel(MatrixInverseData.p).genIdentity(2) == MatrixInverseData.ident2x2)
   }
   it should "Generate append Matrix 2x2" in {
-    assert(MatrixInverseModel.append(MatrixInverseData.in2x2, MatrixInverseModel.genIdentity(2))== MatrixInverseData.out2x4)
+    assert(new MatrixInverseModel(MatrixInverseData.p).append(MatrixInverseData.in2x2, new MatrixInverseModel(MatrixInverseData.p).genIdentity(2))== MatrixInverseData.out2x4)
   }
-//   it should "gauss 2x2" in {
-//     assert(MatrixInverseModel.gauss(MatrixInverseModel.append(MatrixInverseData.in2x2, MatrixInverseModel.genIdentity(2)))== MatrixInverseData.out2x4)
-//   }
   it should("gauss") in {
-      assert((MatrixInverseModel.gaussElimination(MatrixInverseData.in2x2, MatrixInverseModel.genIdentity(2))== MatrixInverseData.outgauss2x4))
+      assert((new MatrixInverseModel(MatrixInverseData.p).gaussElimination(MatrixInverseData.in2x2, new MatrixInverseModel(MatrixInverseData.p).genIdentity(2))== MatrixInverseData.outgauss2x4))
   }
 
 }
